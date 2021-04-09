0e0d779b95486453ca09b8e9a1767cda2fa92963
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-04-09 15:53:07 +0100

    Implemented relevant project images and descriptions

--- src/App/App.js
+++ src/App/App.js
@@ -6,7 +6,9 @@ import About from "../components/About/About";
 import Cards from "../components/Cards/Cards";
 import Contact from "../components/Contact/Contact";
 import "./App.css";
-import Mobile from "../snapshots/Coin_Details_Mobile.jpg";
+import CointTossMobile from "../snapshots/Landing_Page_Mobile.jpg";
+import PocketStocksMobile from "../snapshots/Pocket_Stocks_Mobile.JPG";
+import RentFlow from "../snapshots/Rent_Flow.JPG";
 
 function App() {
   return (
@@ -17,19 +19,19 @@ function App() {
       <div className="projects-cards">
         <Cards
           title="Coin Toss"
-          imageUrl={Mobile}
+          imageUrl={CointTossMobile}
           body="Coin Toss is an app that provides users a list of the top seven trending cryptocurrencies on Coin Gecko. Data retrieved via the Coin Gecko API is displayed on each listed coins 'detail' page. After reading and analyzing this data, users can then cast their vote whether they believe the coin is worth purchasing."
           link={"https://cointoss-client.vercel.app/"}
         />
         <Cards
           title="Pocket Stocks"
-          imageUrl={Mobile}
+          imageUrl={PocketStocksMobile}
           body="Pocket Stocks is a PERN stack application which allows users to view trending stocks and discuss their performance."
           link={"https://pocket-stocks-client.vercel.app/"}
         />
         <Cards
           title="Rent Flow"
-          imageUrl={Mobile}
+          imageUrl={RentFlow}
           body="Rent-flow provides users the ability to search for rental property comps in relation to a specified address. Users can then use the monthly cash-flow calculator to determine their expected profits."
           link={"https://dvanpelt-hub.github.io/Rent-Flow/"}
         />
