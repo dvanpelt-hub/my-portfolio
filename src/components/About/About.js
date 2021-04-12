52126e08f0df270318c4cc959698b1fd7cf672f6
Daniel Vanpelt <vanpelt.daniel@yahoo.com>
2021-04-12 14:49:54 +0100

    Finalized response layout for About component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,29 +7,35 @@ export default function About() {
   return (
     <>
       <div className="aboutContainer" id="about">
-        <div className="imageContainer">
+        <div className="aboutContent">
           <h2>Who am I?</h2>
-          <img
-            className="profile"
-            src={Profile_photo}
-            alt="Daniel Vanpelt face"
-          />
-        </div>
-        <div className="aboutInfo">
-          <p>
-            A full-stack web developer and Thinkful bootcamp graduate who is
-            proficient in HTML5, CSS3, React, and NodeJS. Over the past nine
-            months, I have worked on several PERN stack applications which has
-            led to the growth of both my front and back-end capabilities.
-          </p>
-          <p>
-            Prior to my career change, I served in the US Army and worked
-            extensively as a government and military contractor. Throughout the
-            past 10 years, I participated in multiple overseas deployments and
-            became an adaptable and effective analyst within the Intelligence
-            community. This experience rewarded me with an extensive set of
-            analytical, technical, and training skills.
-          </p>
+          <hr />
+          <div className="infoContainer">
+            <div className="imageContainer">
+              <img
+                className="profile"
+                src={Profile_photo}
+                alt="Daniel Vanpelt face"
+              />
+            </div>
+            <div className="aboutInfo">
+              <p>
+                A full-stack web developer and Thinkful bootcamp graduate who is
+                proficient in HTML5, CSS3, React, and NodeJS. Over the past nine
+                months, I have worked on several PERN stack applications which
+                has led to the growth of both my front and back-end
+                capabilities.
+              </p>
+              <p>
+                Prior to my career change, I served in the US Army and worked
+                extensively as a government and military contractor. Throughout
+                the past 10 years, I participated in multiple overseas
+                deployments and became an adaptable and effective analyst within
+                the Intelligence community. This experience rewarded me with an
+                extensive set of analytical, technical, and training skills.
+              </p>
+            </div>
+          </div>
         </div>
         <div className="view-my-projects-btn-container">
           <button className="view-my-projects-btn">
